d0997bf2ff91e7308119725a540623223ff8b1f6
n27 <skfdkfkalsrn@gmail.com>
2022-03-25 17:24:05 +0900

    route v6

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Link, Route, Routes } from 'react-router-dom';
+import About from './About';
+import Home from './Home';
+import Profiles from './Profiles';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+        <li>
+          <Link to='/profiles'>프로필</Link>
+        </li>
+        <li>
+          <Link to='/history'>History 예제</Link>
+        </li>
+      </ul>
+
+      <hr />    
+      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profiles/*' element={<Profiles />} />        
+      </Routes>
+    
     </div>
   );
 }
 
-export default App;
+export default App
